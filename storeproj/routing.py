cc5a6c496f39272e0fd9b4cfb6902edd6ccde88e
pk-pythondeveloper <pk5999972@gmail.com>
2026-01-15 23:21:13 +0530

    i am adding new   commernt

--- storeproj/routing.py
+++ storeproj/routing.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from shop import consumers
 
-# accept ws/shop and ws/shop/ (trailing slash optional)
 websocket_urlpatterns = [
-    re_path(r'ws/shop/?$', consumers.ShopConsumer.as_asgi()),
+    re_path(r'ws/shop/$', consumers.ShopConsumer.as_asgi()),
 ]
+#i am adding comment here
